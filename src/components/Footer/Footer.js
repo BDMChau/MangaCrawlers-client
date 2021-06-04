ac1b2163c5f0c992eb82509b332ba51f1e47922f
MChau105 <bdmchau10005@gmail.com>
2021-06-04 22:01:08 +0700

    Chau_footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { Col, Row, Typography } from 'antd'
 import { Footer } from 'antd/lib/layout/layout'
-import React from 'react'
 import "./Footer.css"
+import dayjs from "dayjs";
+
 
 export default function FooterContainer() {
+    const [time, setTime] = useState(dayjs().millisecond())
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <Footer className="footer">
-            This is footer
+            <div span={24} className="footer-bg">
+            </div>
+            <Row justify={"center"} className="footer-row">
+                <Col span={24} className="col01">
+                    <div className="footer-logo"></div>
+                    <div className="footer-text">
+                        <Typography.Text>Copyright © 2021 MangaCrawlers</Typography.Text>
+
+                        <Typography.Text style={{ display: "block" }}>
+                            Copyrights and trademarks for the manga, and other promotional materials are held by their respective owners and their use is allowed under the fair use clause of the Copyright Law. © 2021 MangaCrawlers.  <br />
+                            Our Email: mangacrawlers123@gmail.com
+                        </Typography.Text>
+                    </div>
+                </Col>
+            </Row>
+
         </Footer>
     )
 }
