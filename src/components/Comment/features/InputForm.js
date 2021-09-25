9385af2687c94a39942eea638edb89528be1dbfa
MChau105 <bdmchau10005@gmail.com>
2021-09-25 17:12:19 +0700

    .

--- src/components/Comment/features/InputForm.js
+++ src/components/Comment/features/InputForm.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "../CommentItems/CommentItems.css"
 
 import { Button, Col, Form, Input } from 'antd'
+import ContentEditable from 'react-contenteditable'
 
 const { TextArea } = Input;
 
-export default function InputForm({ isAddedCmt, setIsAddedCmt, addCmt}) {
+export default function InputForm({ isAddedCmt, setIsAddedCmt, addCmt }) {
     const [cmtContent, setCmtContent] = useState("");
     const [isAdding, setIsAdding] = useState(false);
 
@@ -19,28 +20,34 @@ export default function InputForm({ isAddedCmt, setIsAddedCmt, addCmt}) {
 
 
     const handleAddCmt = async () => {
-       if(!cmtContent){
-           return;
-       }
+        if (!cmtContent) {
+            return;
+        }
 
-       setIsAdding(true);
-       await addCmt(cmtContent);
-       setIsAdding(false);
+        setIsAdding(true);
+        await addCmt(cmtContent);
+        setIsAdding(false);
     }
 
 
     return (
         <Form className="form-input">
             <Form.Item>
-                <TextArea
-                    className="input"
-                    type="text"
-                    placeholder="Write a comment..."
-                    value={cmtContent}
-                    onChange={(e) => setCmtContent(e.target.value)}
-                />
+                <div>
+                    <TextArea
+                        className="input"
+                        type="text"
+                        placeholder="Write a comment..."
+                        value={cmtContent}
+                        onChange={(e) => setCmtContent(e.target.value)}
+                    />
+
+                    <div className="bottom-container">
+
+                    </div>
+                </div>
             </Form.Item>
-            
+
             <Form.Item>
                 <Button className="btn-submit" type="primary" loading={isAdding} onClick={() => handleAddCmt()}>
                     Add Comment
