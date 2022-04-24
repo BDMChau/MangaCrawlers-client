e30f26b2b7196c3976f16739bf5bc3a35949a80f
MChau105 <bdmchau10005@gmail.com>
2022-04-24 17:11:10 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppService from './App/AppService';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <AppService />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
